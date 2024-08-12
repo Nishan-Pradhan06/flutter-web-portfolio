d15984b922ec02411d827fdb4bb7bd236f1a46ce
nishanpradhan <nishanpradhan500@gmail.com>
2024-08-12 17:50:18 +0545

    ui-updated

--- lib/component/utils/constants_url_lanchur.dart
+++
@@ -1,4 +0,0 @@
-import 'package:url_launcher/url_launcher.dart';
-
-// URL Launcher
-void openURL(String url) => launchUrl(Uri.parse(url));
--- lib/component/utils/string.dart
+++
@@ -1,2 +0,0 @@
-String aboutme =
-    " Hi! I am Nishan Pradhan from Morang, Nepal. I have recently completed my plus 2 education and am currently \nstudying for my Bachelor's degree in Computer Application (BCA). Alongside my studies, I have been learning \nabout Flutter cross-platform application development. I am passionate about technology and  have been working \non a project related to app development using flutter. I am excited to continue learning and growing in this field \nand hope to make a positive impact in the world of technology.";
